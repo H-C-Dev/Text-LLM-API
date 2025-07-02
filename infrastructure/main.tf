cf64e3a55df00ab2e22db8078a432ddaf9ac22df
thchan1992 <chantszhang@gmail.com>
2025-07-02 17:53:17 +0100

    Fix: Fixed unable to complete the apt update install and pip3 by allowing all ountbound traffic: Added egress SG rule.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -71,6 +71,14 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    description = "Allow all outbound traffic"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
 
 resource "aws_instance" "text_sum" {
