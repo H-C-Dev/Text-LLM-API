8a8afd0fecd88bf361678e8abaff98998e40b1a5
thchan1992 <chantszhang@gmail.com>
2025-07-02 18:37:29 +0100

    Infra: Provisioning the EC2 VM by installing Docker

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -88,6 +88,18 @@ resource "aws_instance" "text_sum" {
   vpc_security_group_ids = [aws_security_group.sg.id]
   key_name               = var.key_name
   associate_public_ip_address = true
+  # user_data = <<-EOF
+  #             #!/bin/bash
+  #             sudo apt-get update -y
+  #             sudo apt-get update -y
+  #             sudo ufw app list
+  #             sudo ufw allow OpenSSH
+  #             sudo ufw allow 80
+  #             sudo ufw --force enable
+  #             sudo ufw status
+  #             sudo apt install python3
+  #             sudo apt install python3-pip -y
+  #           EOF
 
   user_data = <<-EOF
               #!/bin/bash
@@ -100,5 +112,15 @@ resource "aws_instance" "text_sum" {
               sudo ufw status
               sudo apt install python3
               sudo apt install python3-pip -y
+              sudo apt-get install ca-certificates curl
+              sudo install -m 0755 -d /etc/apt/keyrings
+              sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+              sudo chmod a+r /etc/apt/keyrings/docker.asc
+               echo \
+                "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+                $(. /etc/os-release && echo "$${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+                sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+              sudo apt-get update -y
+              sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
               EOF
 }
