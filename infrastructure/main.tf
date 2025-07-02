f465937e79da48de17da4629c1fff0e66a6df324
thchan1992 <chantszhang@gmail.com>
2025-07-02 16:55:08 +0100

    Infra: Provision EC2 with port 80 and 22 for http and ssh connection, and show the output of the EC2 public address.

---
+++ infrastructure/main.tf
@@ -0,0 +1,83 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = var.region
+    profile = var.aws_profile
+}
+
+locals {
+  suffix = formatdate("YYYYMMDDhhmmss", timestamp())
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = var.cidr_block
+  tags = {
+    Name = "${var.project_name}-${local.suffix}"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public_subnet_cidr_block
+  map_public_ip_on_launch = true
+  availability_zone       = var.availability_zone
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = var.project_name
+  }
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = var.route_table_cidr_block
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rt" {
+  route_table_id = aws_route_table.rt.id
+  subnet_id      = aws_subnet.public_subnet.id
+}
+
+resource "aws_security_group" "sg" {
+  name        = var.project_name
+  description = "SG group: only port 80 and 22 for now."
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_instance" "text_sum" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  key_name               = var.key_name
+  associate_public_ip_address = true
+}
---
+++ infrastructure/output.tf
@@ -0,0 +1,5 @@
+output "instance_public_ip" {
+    value = aws_instance.text_sum.public_ip
+}
+
+
